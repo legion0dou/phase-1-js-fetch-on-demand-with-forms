f0cc847b715728e3f25faa540bc02b26638621a6
user_name <email_id>
2023-10-07 01:10:12 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-const init = () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const inputForm = document.getElementById('movieForm');
+    const movieDetails = document.getElementById('movieDetails');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.getElementById('searchByID').value;
+  
+      fetch(`http://localhost:3000/movies/${input}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.id) {
+            // Display movie details
+            movieDetails.innerHTML = `
+              <h4>${data.title}</h4>
+              <p>${data.summary}</p>
+            `;
+          } else {
+            // Handle movie not found
+            movieDetails.innerHTML = `<p>Movie not found</p>`;
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    });
+  });
+  
